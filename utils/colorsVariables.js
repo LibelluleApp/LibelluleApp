d8f911af5b0c0b6e26e325d31cdb8615c6acec71
ArnaudGct <arnaud.graciet@gmail.com>
2024-08-13 10:23:32 +0200

    chore: Update color variables in utils and update npm dependencies

--- utils/colorsVariables.js
+++ utils/colorsVariables.js
@@ -1,13 +1,57 @@
 export const COLORS_LIGHT = {
   background: "#F4F5F9",
+  white_background: "#FFFFFF",
+
   grey: "#9AA5B3",
+  grey_variable: "#9AA5B3",
   white: "#FFFFFF",
   black50: "#385768",
   black: "#18313F",
+  text_placeholder: "#9AA5B3",
+  text_value: "#18313F",
+
+  blue: "#0760FB",
   blue700: "#0760FB",
   blue70050: "#619AFE",
   blue900: "#0F4199",
   blue90075: "#345496",
+
+  green: "#52BB00",
   green700: "#52BB00",
-  green900: "#",
+  green900: "#12841D",
+
+  red: "#E80D0D",
+  red700: "#E80D0D",
+  red900: "#BB0000",
+
+  orange: "BB9200",
+};
+
+export const COLORS_DARK = {
+  background: "#10162A",
+  white_background: "#101D47",
+
+  grey: "#9AA5B3",
+  grey_variable: "#FFFFFF",
+  white: "#FFFFFF",
+  black50: "#E2E2E2",
+  black: "#F6F6F6",
+  text_placeholder: "#9AA5B3",
+  text_value: "#18313F",
+
+  blue: "#68AEFF",
+  blue700: "#0760FB",
+  blue70050: "#619AFE",
+  blue900: "#0F4199",
+  blue90075: "#345496",
+
+  green: "#BCFF87",
+  green700: "#52BB00",
+  green900: "#12841D",
+
+  red: "#FF9D9D",
+  red700: "#E80D0D",
+  red900: "#BB0000",
+
+  orange: "BB9200",
 };
