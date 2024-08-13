0bb3032b868d633d71ad794f06978b45e0f7e56c
Raphitpt <tiphonetraphi@gmail.com>
2024-08-13 10:16:27 +0200

    chore: Add initial color variables to utils

---
+++ utils/colorsVariables.js
@@ -0,0 +1,13 @@
+export const COLORS_LIGHT = {
+  background: "#F4F5F9",
+  grey: "#9AA5B3",
+  white: "#FFFFFF",
+  black50: "#385768",
+  black: "#18313F",
+  blue700: "#0760FB",
+  blue70050: "#619AFE",
+  blue900: "#0F4199",
+  blue90075: "#345496",
+  green700: "#52BB00",
+  green900: "#",
+};
