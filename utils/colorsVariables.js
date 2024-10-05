2eb5c7ef3b742e47fbfe1323ead38b43e39d2fac
ArnaudGct <arnaud.graciet@gmail.com>
2024-10-05 15:50:38 +0200

    chore: change files colors variables

--- utils/colorsVariables.js
+++ utils/colorsVariables.js
@@ -1,5 +1,5 @@
 export const lightModeColors = {
-  background: "#F4F5F9",
+  background: "#F7FAFF",
   white_background: "#FFFFFF",
 
   grey: "#9AA5B3",
@@ -28,11 +28,37 @@ export const lightModeColors = {
   red_background: "#f5dee2",
   red700: "#E80D0D",
   red900: "#BB0000",
+
+  // Blue Ribbon
+  blue50: "#EDF8FF",
+  blue100: "#D6EDFF",
+  blue200: "#B7E1FF",
+  blue300: "#85D1FF",
+  blue400: "#4BB5FF",
+  blue500: "#2293FF",
+  blue600: "#0A72FF",
+  blue700: "#0760FB",
+  blue800: "#0B49C2",
+  blue900: "#0F4199",
+  blue950: "#0F295C",
+
+  // Red
+  red50: "#FFF1F1",
+  red100: "#FFDFDF",
+  red200: "#FFC4C4",
+  red300: "#FF9B9B",
+  red400: "#FF6161",
+  red500: "#FF3030",
+  red600: "#E80D0D",
+  red700: "#CC0909",
+  red800: "#A80C0C",
+  red900: "#8B1111",
+  red950: "#4C0303",
 };
 
 export const darkModeColors = {
   background: "#10162A",
-  white_background: "#101D47",
+  white_background: "#0F295C",
 
   grey: "#9AA5B3",
   grey_variable: "#FFFFFF",
@@ -60,4 +86,30 @@ export const darkModeColors = {
   red_background: "#271527",
   red700: "#E80D0D",
   red900: "#BB0000",
+
+  // Blue Ribbon
+  blue50: "#1E5780",
+  blue100: "#D6EDFF",
+  blue200: "#0F295C",
+  blue300: "#85D1FF",
+  blue400: "#4BB5FF",
+  blue500: "#2293FF",
+  blue600: "#4BB5FF",
+  blue700: "#4BB5FF",
+  blue800: "#D6EDFF",
+  blue900: "#D6EDFF",
+  blue950: "#EDF8FF",
+
+  // Red
+  red50: "#4C0303",
+  red100: "#FFDFDF",
+  red200: "#FFC4C4",
+  red300: "#A80C0C",
+  red400: "#FF6161",
+  red500: "#FF9B9B",
+  red600: "#E80D0D",
+  red700: "#CC0909",
+  red800: "#A80C0C",
+  red900: "#8B1111",
+  red950: "#4C0303",
 };
