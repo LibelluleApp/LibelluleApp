2e0376638593ea342535213ec6fc9dc26d1bceff
ArnaudGct <arnaud.graciet@gmail.com>
2024-10-07 18:37:39 +0200

    fix: navbar height

--- stacks/TabsStack.js
+++ stacks/TabsStack.js
@@ -13,10 +13,17 @@ const TabsStack = () => {
   const { colors } = useContext(ThemeContext);
   const insets = useSafeAreaInsets();
   const [headerTitle, setHeaderTitle] = useState("Vue d'ensemble");
-  const getIcon = (Icon, color, strokeWidth, focused) => {
+  const getIcon = (Icon, color, strokeWidth, focused, height, width) => {
     const fillColor = focused ? colors.blue700 : color;
 
-    return <Icon stroke={fillColor} strokeWidth={strokeWidth} />;
+    return (
+      <Icon
+        stroke={fillColor}
+        strokeWidth={strokeWidth}
+        height={height}
+        width={width}
+      />
+    );
   };
   const changeHeaderTitle = (name) => {
     setHeaderTitle(name);
@@ -29,7 +36,8 @@ const TabsStack = () => {
       options: {
         headerShown: false,
         tabBarLabel: "Accueil",
-        tabBarIcon: ({ color, size, focused }) => getIcon(Home, color, 1.75, focused),
+        tabBarIcon: ({ color, size, focused, height, width }) =>
+          getIcon(Home, color, 1.75, focused, 24, 24),
       },
     },
     {
@@ -38,7 +46,8 @@ const TabsStack = () => {
       options: {
         headerShown: false,
         tabBarLabel: "EDT",
-        tabBarIcon: ({ color, size, focused }) => getIcon(Calendar, color, 1.75, focused),
+        tabBarIcon: ({ color, size, focused, height, width }) =>
+          getIcon(Calendar, color, 1.75, focused, 24, 24),
       },
     },
     {
@@ -47,7 +56,8 @@ const TabsStack = () => {
       options: {
         headerShown: false,
         tabBarLabel: "Devoirs",
-        tabBarIcon: ({ color, size, focused }) => getIcon(Check, color, 1.75, focused),
+        tabBarIcon: ({ color, size, focused, height, width }) =>
+          getIcon(Check, color, 1.75, focused, 24, 24),
       },
     },
     {
@@ -56,47 +66,50 @@ const TabsStack = () => {
       options: {
         headerShown: false,
         tabBarLabel: "Mails",
-        tabBarIcon: ({ color, size, focused }) => getIcon(Envelope, color, 1.75, focused),
+        tabBarIcon: ({ color, size, focused, height, width }) =>
+          getIcon(Envelope, color, 1.75, focused, 24, 24),
       },
-    }
+    },
   ];
 
   return (
-      <View style={{ flex: 1 }}>
-        <HeaderBar title={headerTitle} />
-        <Tab.Navigator
-            screenOptions={{
-              animation: "shift",
-              headerShown: false,
-              tabBarShowLabel: true,
-              tabBarStyle: {
-                paddingHorizontal: 8,
-                backgroundColor: colors.white_background,
-                borderTopWidth: 0,
-                  height: 80,
-              },
-              tabBarActiveTintColor: colors.blue700,
-              tabBarInactiveTintColor: colors.grey,
-              tabBarLabelStyle: {
-                fontFamily: "Ubuntu_500Medium",
-                fontSize: 13,
-              },
-              headerLeft: () => <LogoTitle />,
+    <View style={{ flex: 1 }}>
+      <HeaderBar title={headerTitle} />
+      <Tab.Navigator
+        screenOptions={{
+          animation: "shift",
+          headerShown: false,
+          tabBarShowLabel: true,
+          tabBarStyle: {
+            paddingTop: 8,
+            paddingHorizontal: 15,
+            backgroundColor: colors.white_background,
+            borderTopWidth: 0,
+            height: 95,
+          },
+          tabBarActiveTintColor: colors.blue700,
+          tabBarInactiveTintColor: colors.grey,
+          tabBarLabelStyle: {
+            marginTop: 1,
+            fontFamily: "Ubuntu_500Medium",
+            fontSize: 12,
+          },
+          headerLeft: () => <LogoTitle />,
+        }}
+      >
+        {views.map((view) => (
+          <Tab.Screen
+            key={view.name}
+            name={view.name}
+            component={view.component}
+            options={view.options}
+            listeners={{
+              tabPress: () => changeHeaderTitle(view.name),
             }}
-        >
-          {views.map((view) => (
-              <Tab.Screen
-                  key={view.name}
-                  name={view.name}
-                  component={view.component}
-                  options={view.options}
-                  listeners={{
-                    tabPress: () => changeHeaderTitle(view.name),
-                  }}
-              />
-          ))}
-        </Tab.Navigator>
-      </View>
+          />
+        ))}
+      </Tab.Navigator>
+    </View>
   );
 };
 
