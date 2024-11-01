a61e21dcbfbd2059395b4b154b5d84d02c855347
ArnaudGct <arnaud.graciet@gmail.com>
2024-11-01 18:59:12 +0100

    fix: custom message splash screen

--- context/AuthStackSwitcher.js
+++ context/AuthStackSwitcher.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useContext } from "react";
 import { ActivityIndicator, Text, View, StyleSheet, Image } from "react-native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import { useSession } from "./AuthContext";
@@ -7,12 +7,14 @@ import AuthStack from "../stacks/AuthStack";
 import OfflineScreen from "../views/Offline/Offline";
 import NetInfo from "@react-native-community/netinfo";
 import refreshData from "../api/User/refreshData";
+import { ThemeContext } from "../utils/themeContext";
 
 const Stack = createNativeStackNavigator();
 
 const AuthStackSwitcher = () => {
   const { session, loading } = useSession();
   const [isOnline, setIsOnline] = useState(true);
+  const { colors } = useContext(ThemeContext);
 
   useEffect(() => {
     const unsubscribe = NetInfo.addEventListener((state) => {
@@ -32,12 +34,25 @@ const AuthStackSwitcher = () => {
       alignItems: "center",
       backgroundColor: "#0760FB",
     },
+    loadingContainer: {
+      flexDirection: "row",
+      gap: 10,
+      justifyContent: "center",
+      alignItems: "center",
+    },
     imgContainer: {
       marginBottom: 20,
     },
     img: {
       width: 350,
       height: 350,
+    },
+    imgTitle: {
+      color: colors.white,
+      fontSize: 17,
+      fontFamily: "Ubuntu_400Regular",
+      letterSpacing: -0.4,
+      alignSelf: "center",
     },
   });
   if (loading) {
@@ -48,20 +63,13 @@ const AuthStackSwitcher = () => {
             source={require("../assets/adaptive-icon.png")}
             style={styles.img}
           />
-          <Text
-            style={{
-              color: "#fff",
-              fontSize: 20,
-              fontFamily: "Ubuntu_500Medium",
-              letterSpacing: -0.4,
-              alignSelf: "center",
-              marginBottom: 20,
-            }}
-          >
-            Chargement...
-          </Text>
+          <View style={styles.loadingContainer}>
+            <ActivityIndicator size="small" color={colors.white} />
+            <Text style={styles.imgTitle}>
+              Libellule s’éveille doucement...
+            </Text>
+          </View>
         </View>
-        <ActivityIndicator size="large" color={"#fff"} />
       </View>
     );
   }
